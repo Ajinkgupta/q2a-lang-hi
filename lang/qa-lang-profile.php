68e975d555d5899ca504eb9a1f42e674b7089691
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-10-26 10:37:10 +0530

    Create qa-lang-profile.php

---
+++ lang/qa-lang-profile.php
@@ -0,0 +1,91 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	Description: Language phrases for user profile page
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+return array(
+'1_chosen_as_best' => ' (1 को सर्वश्रेष्ठ चुना गया)',
+'1_down_vote' => '1 डाउन वोट',
+'1_up_vote' => '1 अधिक वोट',
+'1_with_best_chosen' => ' (सर्वश्रेष्ठ उत्तर वाला 1 चुना गया)',
+'activity_by_x' => 'गतिविधि ^ द्वारा',
+'answers' => 'उत्तर:',
+'answers_by_x' => 'उत्तर ^',
+'bonus_points' => 'बोनस पॉइंट्स:',
+'comments' => 'टिप्पणियाँ:',
+'delete_pm_popup' => 'यह निजी संदेश हटाएं',
+'delete_wall_post_popup' => 'यह वॉल पोस्ट हटाएं',
+'extra_privileges' => 'अतिरिक्त विशेषाधिकार:',
+'gave_out' => 'बाहर दिया:',
+'my_account_title' => 'मेरा खाता',
+'no_answers_by_x' => '^ द्वारा कोई उत्तर नहीं',
+'no_posts_by_x' => '^ द्वारा कोई पोस्ट नहीं',
+'no_questions_by_x' => '^ द्वारा कोई प्रश्न नहीं',
+'permit_anon_view_ips' => 'गुमनाम पोस्ट के आईपी देखना',
+'permit_close_q' => 'कोई भी प्रश्न बंद करना',
+'permit_delete_hidden' => 'छिपे हुए पोस्ट हटा रहा हूँ',
+'permit_edit_a' => 'किसी भी उत्तर का संपादन',
+'permit_edit_c' => 'किसी भी टिप्पणी का संपादन',
+'permit_edit_q' => 'किसी भी प्रश्न का संपादन',
+'permit_edit_silent' => 'चुपचाप पोस्ट संपादित करना',
+'permit_flag' => 'पोस्टों को ध्वजांकित करना',
+'permit_hide_show' => 'किसी भी पोस्ट को छिपाना या दिखाना',
+'permit_moderate' => 'पोस्ट को स्वीकृत या अस्वीकार करना',
+'permit_post_a' => 'प्रश्नों का उत्तर देना',
+'permit_post_c' => 'टिप्पणियाँ जोड़ना',
+'permit_post_q' => 'प्रश्न पूछना',
+'permit_post_wall' => 'उपयोगकर्ता दीवारों पर पोस्ट करना',
+'permit_recat' => 'किसी भी प्रश्न को पुनः वर्गीकृत करना',
+'permit_retag' => 'किसी भी प्रश्न को पुनः टैग करना',
+'permit_select_a' => 'किसी भी प्रश्न का उत्तर चुनना',
+'permit_view_q_page' => 'प्रश्न पृष्ठ देखना',
+'permit_view_new_users_page' => 'नवीनतम उपयोगकर्ता पृष्ठ देखना',
+'permit_view_special_users_page' => 'विशेष उपयोगकर्ता पृष्ठ देखना',
+'permit_view_voters_flaggers' => 'यह देखना कि किसने वोट किया या पोस्ट को फ़्लैग किया',
+'permit_vote_a' => 'उत्तरों पर मतदान',
+'permit_vote_c' => 'टिप्पणियों पर मतदान',
+'permit_vote_down' => 'वोटिंग पोस्ट बंद',
+'permit_vote_q' => 'प्रश्नों पर मतदान',
+'post_wall_blocked' => 'इस उपयोगकर्ता ने अपनी वॉल पर नए पोस्ट की अनुमति नहीं दी है',
+'post_wall_button' => 'वॉल पोस्ट जोड़ें',
+'post_wall_empty' => 'कृपया इस वॉल पर पोस्ट करने के लिए कुछ दर्ज करें',
+'post_wall_limit' => 'आप इस घंटे में और अधिक वॉल पोस्ट नहीं लिख सकते',
+'post_wall_must_be_approved' => 'इस वॉल पर पोस्ट करने के लिए आपका खाता स्वीकृत होना चाहिए। कृपया प्रतीक्षा करें या ^1अधिक जानकारी जोड़ें^2.',
+'post_wall_must_confirm' => 'कृपया ^5इस वॉल पर पोस्ट करने के लिए अपने ईमेल पते^6 की पुष्टि करें।',
+'post_wall_must_login' => 'इस वॉल पर पोस्ट करने के लिए कृपया ^1लॉग इन^2 या ^3रजिस्टर^4 करें।',
+'questions' => 'प्रश्न:',
+'questions_by_x' => 'प्रश्न ^ द्वारा',
+'ranked_x' => '(रैंक #^)',
+'recieved' => 'प्राप्त:',
+'recent_activity_by_x' => 'हाल की गतिविधि ^ द्वारा',
+'score' => 'स्कोर:',
+'send_private_message' => ' - ^1निजी संदेश भेजें^2',
+'set_bonus_button' => 'अपडेट बोनस',
+'title' => 'शीर्षक:',
+'user_x' => 'उपयोगकर्ता ^',
+'user_x_disabled_pms' => 'उपयोगकर्ता ^ ने निजी संदेशों को अक्षम कर दिया है।',
+'voted_on' => 'वोटेड ऑन:',
+'wall_for_x' => 'दीवार ^ के लिए',
+'wall_view_more' => 'अधिक वॉल पोस्ट देखें...',
+'x_chosen_as_best' => '(^ सर्वोत्तम चुना गया)',
+'x_down_votes' => '^ डाउन वोट',
+'x_up_votes' => '^ ऊपर वोट',
+'x_with_best_chosen' => ' (^ सर्वश्रेष्ठ उत्तर के साथ चुना गया)',
+
+);
